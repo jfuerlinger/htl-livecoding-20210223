abb8748f63ee6aeb1da129e18972bf61f251d4c9
Josef Fürlinger <josef.fuerlinger@gmail.com>
2021-02-25 08:38:45 +0100

    Add project files.

---
+++ WebApiDemoWithSwagger.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemoWithSwagger.Api.Controllers
+{
+  /// <summary>
+  /// Provides a simple weather forecast.
+  /// </summary>
+  [ApiController]
+  [Route("/api/[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+      _logger = logger;
+    }
+
+    /// <summary>
+    /// Delivers 5 weather forecasts.
+    /// </summary>
+    /// <returns>the next weather forecasts.</returns>
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+      var rng = new Random();
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+
+    /// <summary>
+    /// Delivers historical data
+    /// </summary>
+    /// <param name="timestamp">a timestamp in the past</param>
+    /// <returns>the weather in the past</returns>
+    [HttpGet("{timestamp}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<WeatherForecast> Get(DateTime timestamp)
+    {
+      if(timestamp >= DateTime.Now)
+      {
+        return NotFound();
+      }
+
+      var rng = new Random();
+      return Ok(new WeatherForecast
+      {
+        Date = timestamp,
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+      });
+    }
+  }
+}
---
+++ WebApiDemoWithSwagger.Api/Startup.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WebApiDemoWithSwagger.Api
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // Register the Swagger generator, defining 1 or more Swagger documents
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Weather Forecast API",
+          Description = "A simple example ASP.NET Core Web API",
+          Contact = new OpenApiContact
+          {
+            Name = "Josef Fürlinger",
+            Email = "j.fuerlinger@htblaleonding.onmicrosoft.com",
+            Url = new Uri("https://twitter.com/jfuerlinger"),
+          },
+          License = new OpenApiLicense
+          {
+            Name = "Use under MIT",
+            Url = new Uri("https://example.com/license"),
+          }
+        });
+
+        // Set the comments path for the Swagger JSON and UI.
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+
+      });
+
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+
+
+        // Enable middleware to serve generated Swagger as a JSON endpoint.
+        app.UseSwagger();
+
+        // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+        // specifying the Swagger JSON endpoint.
+        app.UseSwaggerUI(c =>
+        {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Weather Forecast API V1");
+        });
+
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
